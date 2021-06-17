ab498edd630f839dc92bdcc16478cc926d8bc06d
Padeiro a Fênix <mococs>
2021-06-16 21:41:41 -0300

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./styles/App.css";
 import Header from './components/Header';
 import Categories from './components/Categories';
 import GrowthPlans from './components/GrowthPlans';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Categories />
         <GrowthPlans/>
+        <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <h3>Made with React.JS without any external library.</h3>
+            <a href="https://github.com/mococa/react-page-layout-codeby" target="_blank">
+                <img src="https://image.flaticon.com/icons/png/512/25/25231.png" alt="github" />
+                Github Repo
+            </a>
+        </footer>
+    )
+}
+
+export default Footer
